443303b2dd6c4836532cb24a707d2271a4aed7ff
Yevhenii Rudenko <yerudenko@osdb.io>
2023-10-13 15:48:09 +0300

    N/A: Spring init

---
+++ src/main/java/com/task/Phototeca/PhototecaApplication.java
@@ -0,0 +1,13 @@
+package com.task.Phototeca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhototecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhototecaApplication.class, args);
+	}
+
+}
